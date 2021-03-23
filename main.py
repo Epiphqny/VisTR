87b598832a902f2dde171fcc7a54196a6e52dee0
Yuqing Wang <epiphqny1994925@gmail.com>
2021-03-23 09:59:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def get_args_parser():
     parser.add_argument('--lr_backbone', default=1e-5, type=float)
     parser.add_argument('--batch_size', default=1, type=int)
     parser.add_argument('--weight_decay', default=1e-4, type=float)
-    parser.add_argument('--epochs', default=16, type=int)
-    parser.add_argument('--lr_drop', default=13, type=int)
+    parser.add_argument('--epochs', default=18, type=int)
+    parser.add_argument('--lr_drop', default=12, type=int)
     parser.add_argument('--clip_max_norm', default=0.1, type=float,
                         help='gradient clipping max norm')
 
